ed7eb618741ba1effd72f8ad38d62a27e77aa4de
Chase Mortensen <chase.mortensen3@gmail.com>
2021-04-27 20:31:51 -0600

    adding tailwind table, needs to be refactored

--- smart-task-manager-app/src/App.vue
+++ smart-task-manager-app/src/App.vue
@@ -14,9 +14,10 @@ import Example from './components/Example.vue'
 
 
 const state = reactive({ tasks: [
-  { id: 1, name: 'Task 1' },
-  { id: 2, name: 'Task 2' },
-  { id: 3, name: 'Task 3' },
+  { id: 1, name: 'Pick up groceries', type: 'Shopping', duration: 30, recurring: false, has_deadline: false, can_be_split: false, person_id: 1, state: 0 },
+  { id: 2, name: 'Run 3 miles', type: 'Exercise', duration: 30, recurring: true, has_deadline: false, can_be_split: true, person_id: 1, state: 0 },
+  { id: 3, name: 'Read book', type: 'Personal', duration: 45, recurring: true, has_deadline: false, can_be_split: true, person_id: 1, state: 1 },
+  { id: 3, name: 'Finish work project', type: 'Work', duration: 195, recurring: false, has_deadline: true, can_be_split: true, person_id: 1, state: 0 },  
 ]})
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
