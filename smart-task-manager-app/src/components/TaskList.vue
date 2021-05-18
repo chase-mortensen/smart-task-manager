e9fbcb203ed878246ac9f10c89318d967477d172
Chase Mortensen <chase.mortensen3@gmail.com>
2021-05-18 13:05:05 -0600

    Adding the add task button

--- smart-task-manager-app/src/components/TaskList.vue
+++ smart-task-manager-app/src/components/TaskList.vue
@@ -5,6 +5,13 @@
         class="py-2 align-middle inline-block min-w-full w-auto sm:px-6 lg:px-8"
       >
       <!-- <h2>&nbsp;Task List</h2> -->
+        <button class="justify-center w-1/4">
+          <div class="rounded-md shadow">
+            <span class="w-full flex items-center justify-center px-8 py-4 border border-transparent text-base font-medium rounded-md text-white bg-green-800 hover:bg-green-700 md:py-2 md:text-lg md:px-10">
+              Add Task
+            </span>
+          </div>
+        </button>
         <TaskListSearchBar
           class="min-w-full max-w-full divide-y divide-gray-200"
         />
